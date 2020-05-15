8c861f61562cace6a924c7a01e371b4c90cccfce
Sebastian Brommer <sebastian.brommer@kit.edu>
2020-05-15 17:29:31 +0200

    update em emb binning

--- bin/MorphingSMRun2Legacy.cpp
+++ bin/MorphingSMRun2Legacy.cpp
@@ -433,7 +433,7 @@ int main(int argc, char **argv) {
       auto min = shape.GetBinLowEdge(1);
       if(bstr.Contains("em") && bstr.Contains("misc")) cb.cp().bin({b}).VariableRebin({min, 0.4, 1.0});
       else if(bstr.Contains("em_emb")){
-        if(categories == "stxs_stage1p1") cb.cp().bin({b}).VariableRebin({min, 0.35, 1.0});
+        if(categories == "stxs_stage1p1") cb.cp().bin({b}).VariableRebin({min, 0.3, 1.0});
         else cb.cp().bin({b}).VariableRebin({min, 0.4, 0.5, 0.6, 1.0});
       }
       else if(bstr.Contains("et") && bstr.Contains("misc")) cb.cp().bin({b}).VariableRebin({min, 0.4, 0.5, 0.6, 1.0});
